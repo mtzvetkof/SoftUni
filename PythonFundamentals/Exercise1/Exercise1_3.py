85011ccb73049134dc5558460d8591e80cd717a6
mtsvetkoff <mtzvetkoff@abv.bg>
2020-09-15 23:14:28 +0300

    Python Fundamentals

---
+++ PythonFundamentals/Exercise1/Exercise1_3.py
@@ -0,0 +1,10 @@
+year = int(input())
+
+if year == 88:
+    print('Leo finally won the Oscar! Leo is happy')
+elif year == 86:
+    print('Not even for Wolf of Wall Street?!')
+elif year < 88 and year != 86:
+    print('When will you give Leo an Oscar?')
+elif year > 88:
+    print('Leo got one already!')
