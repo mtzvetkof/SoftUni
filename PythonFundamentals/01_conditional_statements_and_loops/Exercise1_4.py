36cdbb5ec16434bb69cd02de7e92df5ecc48e3a1
mtsvetkoff <mtzvetkoff@abv.bg>
2020-09-16 09:51:18 +0300

    double char & count sheep

---
+++ PythonFundamentals/01_conditional_statements_and_loops/Exercise1_4.py
@@ -0,0 +1,7 @@
+a = str(input())
+len_a = len(a)
+b = ''
+for i in range(0,len_a):
+    b = b + a[i]*2
+print(b)
+
---
+++ PythonFundamentals/01_conditional_statements_and_loops/Exercise1_5.py
@@ -0,0 +1,6 @@
+counter = int(input())
+a = ''
+sheep = ' sheep...'
+for i in range(1, counter+1):
+    a = a + str(i) + sheep
+print(a)
